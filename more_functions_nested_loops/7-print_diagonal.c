9a3832fa9727fcbc3d7ba81658a92547321b4692
denver <denvercayetano23@gmail.com>
2024-11-23 05:21:29 +0000

    test7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,22 +6,22 @@
  */
 void print_diagonal(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n <= 0) /* If n is 0 or less, just print a newline */
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)  /* If n is 0 or less, just print a newline */
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < n; i++) /* Loop n times */
-    {
-        for (j = 0; j < i; j++) /* Print spaces before the '\' */
-        {
-            _putchar(' ');
-        }
-        _putchar('\\'); /* Print the diagonal character */
-        _putchar('\n'); /* Move to the next line */
-    }
+	for (i = 0; i < n; i++)  /* Loop n times */
+	{
+		for (j = 0; j < i; j++)  /* Print spaces before the '\' */
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');  /* Print the diagonal character */
+		_putchar('\n');  /* Move to the next line */
+	}
 }
 
