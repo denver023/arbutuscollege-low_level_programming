7c9ab6c96b1b62ac7689c4ba38fc062b05f26df9
denver <denvercayetano23@gmail.com>
2024-11-23 04:35:02 +0000

    test1

--- more_functions_nested_loops/1-main.c
+++ more_functions_nested_loops/1-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -16,3 +16,4 @@ int main(void)
 	printf("%c: %d\n", c, _isdigit(c));
 	return (0);
 }
+
