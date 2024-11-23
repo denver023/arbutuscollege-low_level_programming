afc5e662d9a8a1f48c24c629e7c3a25b33767ecc
denver <denvercayetano23@gmail.com>
2024-11-23 04:26:00 +0000

    test0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - Checks for uppercase character
  * @c: The character to check
@@ -7,8 +8,7 @@
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
-
