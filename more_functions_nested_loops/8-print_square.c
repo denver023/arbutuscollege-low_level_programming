cfff6ce6bbecb56b76674f555622833d5b5397c8
denver <denvercayetano23@gmail.com>
2024-11-23 05:30:11 +0000

    test8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,21 +6,21 @@
  */
 void print_square(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)  /* If size is 0 or less, print only a newline */
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)  /* If size is 0 or less, print only a newline */
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < size; i++)  /* Loop through rows */
-    {
-        for (j = 0; j < size; j++)  /* Loop through columns */
-        {
-            _putchar('#');  /* Print the square character */
-        }
-        _putchar('\n');  /* Move to the next line after printing a row */
-    }
+	for (i = 0; i < size; i++)  /* Loop through rows */
+	{
+		for (j = 0; j < size; j++)  /* Loop through columns */
+		{
+			_putchar('#');  /* Print the square character */
+		}
+		_putchar('\n');  /* Move to the next line after printing a row */
+	}
 }
 
