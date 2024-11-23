6099dd9dc673b32b13090b5aa74cd1d5aef49e3b
denver <denvercayetano23@gmail.com>
2024-11-23 04:58:17 +0000

    test6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,19 +6,17 @@
  */
 void print_line(int n)
 {
-    int i;
-
-    if (n <= 0)
-    {
-        _putchar('\n');  /* Print only a newline if n is 0 or negative */
-    }
-    else
-    {
-        for (i = 0; i < n; i++)
-        {
-            _putchar('_');  /* Print _ n times */
-        }
-        _putchar('\n');  /* Print newline at the end */
-    }
+	int i;
+	if (n <= 0)
+	{
+		_putchar('\n');  /* Print only a newline if n is 0 or negative */
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');  /* Print _ n times */
+		}
+		_putchar('\n');  /* Print newline at the end */
+	}
 }
-
