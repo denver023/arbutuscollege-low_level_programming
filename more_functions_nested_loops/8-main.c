6e69125d66b31a64d81e222bd13e8e49ac0953cc
denver <denvercayetano23@gmail.com>
2024-11-23 05:25:08 +0000

    test8

---
+++ more_functions_nested_loops/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_square(2);  /* Test case 1: 2x2 square */
+    print_square(10); /* Test case 2: 10x10 square */
+    print_square(0);  /* Test case 3: 0 (should print just a newline) */
+    return (0);
+}
+
---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of '#' characters
+ * @size: the size of the square
+ */
+void print_square(int size)
+{
+    int i, j;
+
+    if (size <= 0)  /* If size is 0 or less, print only a newline */
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (i = 0; i < size; i++)  /* Loop through rows */
+    {
+        for (j = 0; j < size; j++)  /* Loop through columns */
+        {
+            _putchar('#');  /* Print the square character */
+        }
+        _putchar('\n');  /* Move to the next line after printing a row */
+    }
+}
+
