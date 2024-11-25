66faaa10487f720fea1eff1fcfac0846b57d5964
denver <denvercayetano23@gmail.com>
2024-11-25 03:11:21 +0000

    test1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -7,11 +7,10 @@
  */
 void swap_int(int *a, int *b)
 {
-
 	int temp;
 
 	temp = *a; /* Store the value of the first integer in a temporary variable */
-    	*a = *b;   /* Assign the value of the second integer to the first integer */
-    	*b = temp; /* Assign the value of the temporary variable to the second integer */
+	*a= *b;   /* Assign the value of the second integer to the first integer */
+	*b = temp; /* Assign the value of the temporary variable to the second integer */
 }
 
