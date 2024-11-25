e5a6a5e307fa220200418fc17a1e1a240b59d33a
denver <denvercayetano23@gmail.com>
2024-11-25 06:00:30 +0000

    test

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers, followed by a new line
- * @a: pointer to the array
+ * print_array - prints n elements of an array of integers
+ * @a: the array
  * @n: number of elements to print
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", a[i]);
-        if (i < n - 1)  /* Check if it's not the last element */
-        {
-            printf(", ");  /* Print comma and space for all but the last element */
-        }
-    }
-    printf("\n");  /* Print a new line after the array */
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
 
