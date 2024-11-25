af43e508f0cd703eed0402bfb59a917613c525b9
denver023 <denvercayetano23@gmail.com>
2024-11-24 19:16:08 -0800

    Update 1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -10,6 +10,6 @@ void swap_int(int *a, int *b)
 int temp;
 
 	temp = *a; /* Store the value of the first integer in a temporary variable */
-	*a= *b;   /* Assign the value of the second integer to the first integer */
+	*a = *b;   /* Assign the value of the second integer to the first integer */
 	*b = temp; /* Assign the value of the temporary variable to the second integer */
 }
