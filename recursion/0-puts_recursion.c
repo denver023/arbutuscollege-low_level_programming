d3d8f431a3ceccc6d7d6957f2988e937a2dbcba3
denver <denvercayetano23@gmail.com>
2024-12-01 05:24:31 +0000

    test0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') /* Base case: end of string */
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s); /* Print the current character */
-    _puts_recursion(s + 1); /* Recursive call with the next character */
+	if (*s == '\0') /* Base case: end of string */
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s); /* Print the current character */
+	_puts_recursion(s + 1); /* Recursive call with the next character */
 }
 
