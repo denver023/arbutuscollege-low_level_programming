35c4dc043e5b84267850739e3cd833473fd51eb7
denver <denvercayetano23@gmail.com>
2024-12-01 05:45:15 +0000

    tests

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,20 +2,20 @@
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
+	if (n < 0)
+		return (-1);
 
-    return (_sqrt_helper(n, 1));
+	return (_sqrt_helper(n, 1));
 }
 
 int _sqrt_helper(int n, int i)
 {
-    if (i * i > n)
-        return (-1);
+	if (i * i > n)
+		return (-1);
 
-    if (i * i == n)
-        return (i);
+	if (i * i == n)
+		return (i);
 
-    return (_sqrt_helper(n, i + 1));
+	return (_sqrt_helper(n, i + 1));
 }
 
