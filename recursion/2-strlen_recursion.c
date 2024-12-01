fe108ab263b45bb97ab2f5902b5d440edeae28f6
denver <denvercayetano23@gmail.com>
2024-12-01 05:38:51 +0000

    tests

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,9 +8,9 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')  /* Base case: end of the string */
-        return (0);
-    
-    return (1 + _strlen_recursion(s + 1));  /* Recursive call */
+	if (*s == '\0')  /* Base case: end of the string */
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));  /* Recursive call */
 }
 
