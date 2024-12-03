cef22e2100ca96b4b3f9b7db9d1d7329f2bd84c6
denver <denvercayetano23@gmail.com>
2024-12-03 00:40:46 +0000

    0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Pointer to a string for the dog's name
+ * @age: Float for the dog's age
+ * @owner: Pointer to a string for the dog's owner
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
+
