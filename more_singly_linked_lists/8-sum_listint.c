8c94e091cc61f6ccdbe1949f4af04ed34ebcd676
denver023 <denvercayetano23@gmail.com>
2025-01-25 19:51:39 -0800

    More singly linked lists

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -2,9 +2,6 @@
 
 /**
  * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
- * @head: pointer to the first node in the list
- * 
- * Return: the sum of all the data, or 0 if the list is empty
  */
 int sum_listint(listint_t *head)
 {
