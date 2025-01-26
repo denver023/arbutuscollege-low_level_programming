5816449fdd218083ae4dbfa6176acdaad959e9af
denver <denvercayetano23@gmail.com>
2025-01-26 00:28:04 +0000

    Singly linked lists

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
+    size_t len = 0;
 
     new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
@@ -26,7 +27,13 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     }
 
-    new_node->len = strlen(str);
+    /* Calculate string length manually instead of using strlen */
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    new_node->len = len;
     new_node->next = *head;
     *head = new_node;
 
