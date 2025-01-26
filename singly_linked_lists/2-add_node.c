66e79b974b6d0a8a2e5895a6dd249bcc379ec299
denver <denvercayetano23@gmail.com>
2025-01-26 00:31:19 +0000

    Singly linked lists

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,42 +1,44 @@
 #include <stdlib.h>
-#include <string.h>  /* Include this to use strdup */
+#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of the list
- * @head: pointer to the first node in the list
- * @str: string to be added to the new node
+ * add_node - Adds a new node at the beginning of the list
+ * @head: Pointer to the first node in the list
+ * @str: String to be added to the new node
  *
- * Return: address of the new node, or NULL if it failed
+ * Return: Address of the new node, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	tablist_t *new_node;
-	size_t len = 0;
+    list_t *new_node;
+    size_t len = 0;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
 
-	new_node->str = strdup(str); /* This will now work as strdup is declared */
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+    /* Duplicate the string */
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
 
-	/* Calculate string length manually instead of using strlen */
-	while (str[len] != '\0')
-	{
-		len++;
-	}
+    /* Calculate the length of the string manually */
+    while (str[len] != '\0')
+    {
+        len++;
+    }
 
-	new_node->len = len;
-	new_node->next = *head;
-	*head = new_node;
+    new_node->len = len;
+    new_node->next = *head;
+    *head = new_node;
 
-	return (new_node);
+    return (new_node);
 }
 
